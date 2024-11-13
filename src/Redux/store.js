10c1a6fff1224f11502c0fff9d0bf8227e43252c
SharangPm <sharangpm555@gmail.com>
2024-11-13 12:50:27 +0530

    Counter-Redux

---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from './slice/counterSlice'
+
+export const store = configureStore({
+
+    reducer:{
+       counterReducer:counterSlice
+    }
+
+
+    
+})
